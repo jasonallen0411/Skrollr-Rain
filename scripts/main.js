804557975af9327d63321ce3190a6c37c7f607df
Jason Allen <jasonallen@Jasons-MacBook-Pro.local>
2018-05-21 06:27:56 -0500

    Initial Commit

---
+++ scripts/main.js
@@ -0,0 +1,130 @@
+var s = skrollr.init();
+
+
+$("#cloud1").click(function(){
+	var tl = new TimelineMax();
+	tl.to($("#cloud1"), 1, {backgroundColor: "#828A8F"}).delay(-1);
+	tl.to($("#cloud2"), -.25, {backgroundColor: "#828A8F"});
+	tl.to($("#cloud3"), -.5, {backgroundColor: "#828A8F"});
+	tl.to($("#cloud4"), -.75, {backgroundColor: "#828A8F"});
+	tl.to($("#cloud1"), -1, {marginTop: -150});
+	tl.to($("#cloud2"), -1, {marginTop: -200, marginLeft: 100});
+	tl.to($("#cloud3"), -1, {marginTop: -150, marginLeft: 200});
+	tl.to($("#cloud4"), -1, {marginTop: -100, marginLeft: 100});
+	tl.to($("body"), 1, {backgroundColor: "#141436"});
+	$("#hate").css("display", "inline-block");
+	$("#box1").css("display", "block");
+	$("#box2").css("display", "block");
+	$("#box3").css("display", "block");
+	$("#box4").css("display", "block");
+	$("#box5").css("display", "block");
+	$("#box6").css("display", "block");
+	$("#raindrop1").css("display", "block");
+	$("#raindrop2").css("display", "block");
+	$("#raindrop3").css("display", "block");
+	$("#raindrop4").css("display", "block");
+	$("#raindrop5").css("display", "block");
+	$("#raindrop6").css("display", "block");
+	$("#raindrop7").css("display", "block");
+	$("#raindrop8").css("display", "block");
+	$("#click").css("display", "none");
+});
+
+$("#cloud2").click(function(){
+	var tl = new TimelineMax();
+	tl.to($("#cloud1"), 1, {backgroundColor: "#828A8F"}).delay(-1);
+	tl.to($("#cloud2"), -.25, {backgroundColor: "#828A8F"});
+	tl.to($("#cloud3"), -.5, {backgroundColor: "#828A8F"});
+	tl.to($("#cloud4"), -.75, {backgroundColor: "#828A8F"});
+	tl.to($("#cloud1"), -1, {marginTop: -150});
+	tl.to($("#cloud2"), -1, {marginTop: -200, marginLeft: 100});
+	tl.to($("#cloud3"), -1, {marginTop: -150, marginLeft: 200});
+	tl.to($("#cloud4"), -1, {marginTop: -100, marginLeft: 100});
+	tl.to($("body"), 1, {backgroundColor: "#141436"});
+	$("#hate").css("display", "inline-block");
+	$("#box1").css("display", "block");
+	$("#box2").css("display", "block");
+	$("#box3").css("display", "block");
+	$("#box4").css("display", "block");
+	$("#box5").css("display", "block");
+	$("#box6").css("display", "block");
+	$("#raindrop1").css("display", "block");
+	$("#raindrop2").css("display", "block");
+	$("#raindrop3").css("display", "block");
+	$("#raindrop4").css("display", "block");
+	$("#raindrop5").css("display", "block");
+	$("#raindrop6").css("display", "block");
+	$("#raindrop7").css("display", "block");
+	$("#raindrop8").css("display", "block");
+	$("#click").css("display", "none");
+});
+
+$("#cloud3").click(function(){
+	var tl = new TimelineMax();
+	tl.to($("#cloud1"), 1, {backgroundColor: "#828A8F"}).delay(-1);
+	tl.to($("#cloud2"), -.25, {backgroundColor: "#828A8F"});
+	tl.to($("#cloud3"), -.5, {backgroundColor: "#828A8F"});
+	tl.to($("#cloud4"), -.75, {backgroundColor: "#828A8F"});
+	tl.to($("#cloud1"), -1, {marginTop: -150});
+	tl.to($("#cloud2"), -1, {marginTop: -200, marginLeft: 100});
+	tl.to($("#cloud3"), -1, {marginTop: -150, marginLeft: 200});
+	tl.to($("#cloud4"), -1, {marginTop: -100, marginLeft: 100});
+	tl.to($("body"), 1, {backgroundColor: "#141436"});
+	$("#hate").css("display", "inline-block");
+	$("#box1").css("display", "block");
+	$("#box2").css("display", "block");
+	$("#box3").css("display", "block");
+	$("#box4").css("display", "block");
+	$("#box5").css("display", "block");
+	$("#box6").css("display", "block");
+	$("#raindrop1").css("display", "block");
+	$("#raindrop2").css("display", "block");
+	$("#raindrop3").css("display", "block");
+	$("#raindrop4").css("display", "block");
+	$("#raindrop5").css("display", "block");
+	$("#raindrop6").css("display", "block");
+	$("#raindrop7").css("display", "block");
+	$("#raindrop8").css("display", "block");
+	$("#click").css("display", "none");
+});
+
+$("#cloud4").click(function(){
+	var tl = new TimelineMax();
+	tl.to($("#cloud1"), 1, {backgroundColor: "#828A8F"}).delay(-1);
+	tl.to($("#cloud2"), -.25, {backgroundColor: "#828A8F"});
+	tl.to($("#cloud3"), -.5, {backgroundColor: "#828A8F"});
+	tl.to($("#cloud4"), -.75, {backgroundColor: "#828A8F"});
+	tl.to($("#cloud1"), -1, {marginTop: -150});
+	tl.to($("#cloud2"), -1, {marginTop: -200, marginLeft: 100});
+	tl.to($("#cloud3"), -1, {marginTop: -150, marginLeft: 200});
+	tl.to($("#cloud4"), -1, {marginTop: -100, marginLeft: 100});
+	tl.to($("body"), 1, {backgroundColor: "#141436"});
+	$("#hate").css("display", "inline-block");
+	$("#box1").css("display", "block");
+	$("#box2").css("display", "block");
+	$("#box3").css("display", "block");
+	$("#box4").css("display", "block");
+	$("#box5").css("display", "block");
+	$("#box6").css("display", "block");
+	$("#raindrop1").css("display", "block");
+	$("#raindrop2").css("display", "block");
+	$("#raindrop3").css("display", "block");
+	$("#raindrop4").css("display", "block");
+	$("#raindrop5").css("display", "block");
+	$("#raindrop6").css("display", "block");
+	$("#raindrop7").css("display", "block");
+	$("#raindrop8").css("display", "block");
+	$("#click").css("display", "none");
+});
+
+
+
+
+
+
+
+
+
+
+
+
